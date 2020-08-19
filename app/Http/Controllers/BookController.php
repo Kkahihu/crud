07dd5e352347feee1c5c1bea76a7d34c0b3b8594
Kkahihu <kelvinkahihu180226@daystar.ac.ke>
2020-08-19 12:55:59 +0300

    initial commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    //all books
+    public function index()
+    {
+        //
+        $books = Book::all()->toArray();
+        return array_reverse($books);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function add(Request $request)
+    {
+        //add book
+        $book = new Book([
+            'name' => $request->input('name'),
+            'author' => $request->input('author')
+        ]);
+        $books->save();
+
+        return response()->json('The book successfully added');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //edit book
+        $book = Book::find($id);
+        return response()->json($book);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //update book
+        $book = Book::find($id);
+        $book->update($request->all());
+
+        return response()->json('The book successfully updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        //delete book
+        $book = Book::find($id);
+        $book->delete();
+
+        return response()->json('The Book successfully deleted');
+    }
+}
